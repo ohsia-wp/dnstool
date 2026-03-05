0cfeddf04bcf3b88c3d0e738098fc510dab45f50
jsiazar <joseph.siazar@outlook.com>
2026-03-05 05:53:25 +0000

    Remove exposed token

--- assets/footer.php
+++ assets/footer.php
@@ -1,8 +1,12 @@
 <!-- Footer -->
 			<div>
-                <footer id="footer">
-                   <a href="#"> DNS Lookup Tool made by Joseph Siazar | 2024 </a> 
+				<footer id="footer">
+   					<a href="#"> DNS Lookup Tool made by Joseph Siazar | <span id="year"></span> </a>
 				</footer>
+
+				<script>
+					document.getElementById("year").textContent = new Date().getFullYear();
+				</script>
 			</div>
 	</body>
 </html>
